47bd927654ed4f74930df3d417a08d153a025b4c
Kevin Brian <realkevinbrian@gmail.com>
2022-04-16 00:13:19 +0200

    added category table

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TableComponent from "../Table/TableComponent";
+
+function Category() {
+  return (
+    <div>
+      <TableComponent/>
+    </div>
+  )
+}
+
+export default Category
