3d792912d12d9bc80d41bbd3640f1a14a214e66f
Kevin Brian <realkevinbrian@gmail.com>
2022-04-20 21:32:22 +0200

    Funcao de search agora é um componente reusable

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,23 +4,16 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function SearchBar() {
-
-  //set search
-   const [query,setQuery] = useState("");
-
-  //  setSearch
-  
+export default function SearchBar({handleQuery}) {
   return (
     <div>
         <form className='border border-none'>
             <div className="border border-black rounded-full flex justify-around p-0">
                 <input 
-                type="text" 
-                value={query}
+                type="text"
                 className='border border-none outline outline-none bg-transparent pl-2 m-0'
                 placeholder='Pesquisar'
-                onChange={(SearchWord)=>setQuery(SearchWord.target.value.toLowerCase())}
+                onChange={(self)=>handleQuery(self.target.value.toLowerCase())}
                 />
                 <button className='text-lg' type='submit'>
                     <FontAwesomeIcon icon={faSearch}/>
