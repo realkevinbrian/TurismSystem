2ebda0888adde1a236f75d85d7ae1a09a2d630c3
Kevin Brian <realkevinbrian@gmail.com>
2022-04-30 13:35:54 +0200

    New Version Built with styled components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 import {BrowserRouter as Router, Routes,Route} from "react-router-dom";
-import Login from './paginas/Login';
-import AdminPanel from './paginas/AdminPanel';
-import Cadastar from './components/Category/Cadastar';
-
+import Admin from './Pages/Admin';
 
 function App() {
     return ( 
     <Router>
         <div className = "App" >
             <Routes>
-                <Route path='/' element={<Login/>}/>
-                <Route path='/Admin' element={<AdminPanel/>}/>
-                <Route path='/Admin/:category' element={<AdminPanel/>}/>
-                <Route path='/Admin/category/cadastrar' element={<Cadastar/>}/>
+                <Route path='/' element={<Admin/>}/>
             </Routes>
         </div>
     </Router>
