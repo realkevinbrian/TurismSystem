3cb629ba3b1c3c7dd8a505b9a973892d8080da16
Kevin Brian <realkevinbrian@gmail.com>
2022-05-12 22:56:27 +0200

    Fixed logo not displaying bug in Error Page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import * as G from "../Components/Global"
 import {Link} from "react-router-dom"
-import LOGO from "../assets/LOGO"
+import Logo from "../Assets/LOGO.png"
 
 function ErrorPage() {
   return (
     <G.NotFound>
-        <img src={LOGO}/>
+        <img src={Logo} alt="Official Logo"/>
         <G.H5>404 Página não encontrada.</G.H5>
         <Link to="/">Retorne à página anterior</Link>
     </G.NotFound>
